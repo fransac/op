930a6ef199d50f6cf24ecac457f587b7f2940bfa
Francesco Saccone <francesco@francescosaccone.com>
2026-02-03 10:41:11 +0100

    Use Linux GNU cross toolchain prefix

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ ARCH    = riscv64
 MACHINE = virt
 
 # Toolchain.
-CROSS_COMPILE = $(ARCH)-none-elf-
+CROSS_COMPILE = $(ARCH)-linux-gnu-
 AR            = $(CROSS_COMPILE)ar
 AS            = $(CROSS_COMPILE)as
 CC            = $(CROSS_COMPILE)gcc
